e938fde669ff57bfd46cebcc238b28cef467f630
Deepanjal-Uppal <108554919+Deepanjal-Uppal@users.noreply.github.com>
2024-05-19 13:18:37 +0530

    Add files via upload

---
+++ src/EmployeeData.js
@@ -0,0 +1,20 @@
+export const EmployeeData = [
+    {
+        id : 1,
+        firstName : 'Mark',
+        lastName : 'Lee',
+        age : 21
+    },
+    {
+        id : 2,
+        firstName : 'Jackson',
+        lastName : 'Wang',
+        age : 28
+    },
+    {
+        id : 3,
+        firstName : 'Vernon',
+        lastName : 'Hansol',
+        age : 35
+    }
+]
